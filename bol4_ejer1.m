8941bc83a54558d4b6efbdb8d00452e8fc938b21
andreareijat <82641203+andreareijat@users.noreply.github.com>
2021-11-21 16:21:49 +0100

    boletin 3 ejercicio 1

---
+++ bol4_ejer1.m
@@ -0,0 +1,26 @@
+G1=tf([1 -4],[1 1 3 9 16 10]);
+G2=tf([1 8],[1 1 6 2 3]);
+G3=tf([1 3 8],[1 4 8 8 7 4]);
+
+
+% Inestable
+subplot(231)
+step(G1)
+subplot(234)
+pzmap(G1)
+
+% Subamortiguado
+subplot(232)
+step(G2)
+subplot(235)
+pzmap(G2)
+
+% Marginalmente estable
+subplot(233)
+step(G3)
+subplot(236)
+pzmap(G3)
+
+
+
+
