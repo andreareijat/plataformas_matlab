06caa62d4874c57458244270f7638a8e40982b4c
andreareijat <82641203+andreareijat@users.noreply.github.com>
2021-11-21 16:18:58 +0100

    boletin 2 ejercicio 6

---
+++ bol3_ejer6.m
@@ -0,0 +1,13 @@
+%para wn=2 y z=1
+T1=tf([-4 8],[1 2 4]);%s=2
+T2=tf([1 0],[1 2 4]);%s=0
+T3=tf([4 8],[1 2 4]); %s=-2
+T4=tf(4,[1 2 4]);%sin cero
+step(T1)
+hold
+step(T2)
+step(T3)
+step(T4)
+
+%dibujamos los polos y ceros, comentar el hold
+%pzmap(T1,T2,T3,T4)
