ee3b14c678c50c089649dac65ae7404a8f8aa337
andreareijat <82641203+andreareijat@users.noreply.github.com>
2021-12-03 19:42:21 +0100

    Add files via upload

---
+++ c++/ejercicio2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+	int numero = 15;
+	float flotante = 13.23;
+	double doble = 13.4567;
+	char letra = 'a';
+
+	return 0;
+
+}
---
+++ c++/ejercicio3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float numero;
+	cout<<"Introduzca un numero: "<<endl;
+	cin>>numero;
+
+	cout<<numero<<endl;
+
+	return 0;
+}
